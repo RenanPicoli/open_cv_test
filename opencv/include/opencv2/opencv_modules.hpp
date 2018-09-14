a0fff9d66b8dde24af12957e1549fad3feb81d78
Nicolas <nico14142@gmail.com>
2018-09-14 16:03:07 -0300

    Adicionei as bibliotecas da OpenCV, estou desabilitando a compilação do que não interessa. No momento estou com o erro error: 'recursive_mutex' in namespace 'std' does not name a type

---
+++ opencv/include/opencv2/opencv_modules.hpp
@@ -0,0 +1,23 @@
+/*
+ * opencv_modules.hpp
+ *
+ *  Created on: 14 de set de 2018
+ *      Author: estagiario01
+ */
+
+#ifndef OPENCV_MODULES_HPP_
+#define OPENCV_MODULES_HPP_
+
+//Pícoli: adaptei de https://github.com/artoolkit/artoolkit5/blob/master/include/macosx-universal/opencv2/opencv_modules.hpp
+//#define HAVE_OPENCV_CALIB3D
+#define HAVE_OPENCV_CORE
+//#define HAVE_OPENCV_FEATURES2D
+//#define HAVE_OPENCV_FLANN
+#define HAVE_OPENCV_HIGHGUI
+#define HAVE_OPENCV_IMGCODECS
+#define HAVE_OPENCV_IMGPROC
+//#define HAVE_OPENCV_ML
+#define HAVE_OPENCV_VIDEOIO
+
+
+#endif /* OPENCV_MODULES_HPP_ */
---
+++ opencv/modules/core/src/custom_hal.hpp
@@ -0,0 +1,15 @@
+/*
+ * custom_hal.hpp
+ *
+ *  Created on: 14 de set de 2018
+ *      Author: estagiario01
+ */
+
+#ifndef CORE_SRC_CUSTOM_HAL_HPP_
+#define CORE_SRC_CUSTOM_HAL_HPP_
+
+//??????????
+//@_hal_includes@
+
+
+#endif /* CORE_SRC_CUSTOM_HAL_HPP_ */
